632fc2e6ef276b48db8564a8e67a5ea1804c824d
manav-senpai <rockstarsigma@gmail.com>
2026-02-05 23:48:44 +0530

    continuing this project as solo too..

---
+++ Airbnb_Clone/README.md
@@ -0,0 +1,13 @@
+all packages that're needed for this build
+
+npm i express mongoose nodemon bcryptjs cors dotenv jsonwebtoken cookie-parser
+
+MONGODB_URL = "mongodb+srv://manavahire60:Test@123@cluster0.whr8rqh.mongodb.net/
+>> replaced @ in password with %40 
+
+
+installed tailwind css
+
+run these commands in frontend folder 
+>> npm install -D tailwindcss@3 postcss autoprefixer
+>> npx tailwindcss init -p
---
+++ Airbnb_Clone/backend/controllers/auth.controller.js
@@ -0,0 +1,72 @@
+import genToken from "../config/token.js"
+import User from "../model/user.model.js"
+import bcrypt from "bcryptjs"
+
+export const signUp=async (req,res) =>{
+    try{
+        let {name, email, password} = req.body
+        let existUser = await User.findOne({email})
+        if(existUser){
+            return res.status(400).json({message:"User already exists"})
+        }
+        let hashPassword = await bcrypt.hash(password, 10)
+        let user = await User.create({name, email, password:hashPassword})
+        let token = await genToken(user._id)
+        res.cookie("token", token, {
+            httpOnly:true,
+            secure:process.env.NODE_ENVIRONMENT = "production",
+            sameSite : "strict",
+            maxAge : 7 * 24 * 60 * 60 * 1000
+
+
+        })
+        return res.status(201).json(user)
+
+    } catch (error) {
+        return res.status(500).json({message:`signup eror ${error}`})
+    }
+}
+
+export const login = async (req,res) => {
+    try{
+        let {email, password} = req.body
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message:"User does not exist"})
+        }
+        let isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(400).json({message:"Incorrect Password"})
+
+        }
+        let token = await genToken(user._id)
+        res.cookie("token", token, {
+            httpOnly:true,
+            secure:process.env.NODE_ENVIRONMENT = "production",
+            sameSite : "strict",
+            maxAge : 7 * 24 * 60 * 60 * 1000
+
+
+        })
+        return res.status(200).json(user)
+        
+        
+    }
+    catch (error){
+        
+        return res.status(500).json({message:`Login error ${error}`})
+        
+    }
+}
+
+export const logOut = async(req,res) => {
+    try{
+        res.clearCookie("token")
+        return res.status(200).json({message:"Logout Successfully"})
+    }
+    catch(error){
+
+        return res.status(500).json({message:`Logout error ${error}`})
+
+    }
+}
